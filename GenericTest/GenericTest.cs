b5168b71b1737db354296e36821dfb26db90a2fc
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-11 21:46:41 +0530

    Generic Type extends Comparable

--- GenericTest/GenericTest.cs
+++ GenericTest/GenericTest.cs
@@ -6,22 +6,25 @@ namespace GenericTest
 {
     public class GenericTest
     {
-        GenericCheck obj = new GenericCheck();
+        
         [Fact]
         public void GeneticIntTest()
         {
+            GenericCheck<int> obj = new GenericCheck<int>();
             int actual = (obj.Max(22, 33, 90));
             Assert.Equal(90,actual);
         }
         [Fact]
         public void GeneticFloatTest()
         {
+            GenericCheck<double> obj = new GenericCheck<double>();
             double actual = (obj.Max(2.6, 3.3, 8.2));
             Assert.Equal(8.2, actual);
         }
         [Fact]
         public void GeneticStringTest()
         {
+            GenericCheck<String> obj = new GenericCheck<String>();
             String actual = (obj.Max("26", "33", "82"));
             Assert.Equal("82", actual);
         }
