5e4a5aa6f95061bda73f06223be14fa647d729d5
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-11 20:53:20 +0530

    Generic class Created

---
+++ MaximumGeneric/Generic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumGeneric
+{
+    public class Generic
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class GenericCheck<AnyDataType>
+    {
+        public AnyDataType Max(AnyDataType data1, AnyDataType data2, AnyDataType data3)
+        {
+            AnyDataType[] array = { data1, data2, data3 };
+            AnyDataType MaximumValue = array.Max();
+            return MaximumValue;
+        }
+    }
+}
