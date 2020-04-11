4bfd5d9fd1b84c2903f2168f6b67e937250ccd40
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-11 21:51:03 +0530

    Comment Added

--- GenericTest/GenericTest.cs
+++ GenericTest/GenericTest.cs
@@ -6,7 +6,9 @@ namespace GenericTest
 {
     public class GenericTest
     {
-        
+        /// <summary>
+        /// Checking for Generic Integer datatype
+        /// </summary>
         [Fact]
         public void GeneticIntTest()
         {
@@ -14,6 +16,9 @@ namespace GenericTest
             int actual = (obj.Max(22, 33, 90));
             Assert.Equal(90,actual);
         }
+        /// <summary>
+        /// Checking for Generic Float datatype
+        /// </summary>
         [Fact]
         public void GeneticFloatTest()
         {
@@ -21,6 +26,9 @@ namespace GenericTest
             double actual = (obj.Max(2.6, 3.3, 8.2));
             Assert.Equal(8.2, actual);
         }
+        /// <summary>
+        /// Checking for Generic String datatype
+        /// </summary>
         [Fact]
         public void GeneticStringTest()
         {
