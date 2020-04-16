51472169048f37af02e91cf24509af589e151319
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-16 13:42:23 +0530

    Extend Generic method for More then 3

--- GenericTest/GenericTest.cs
+++ GenericTest/GenericTest.cs
@@ -4,7 +4,7 @@ using MaximumGeneric;
 
 namespace GenericTest
 {
-    public class GenericTest
+    public class GenericTest<AnyDataType>
     {
         /// <summary>
         /// Checking for Generic Integer datatype
@@ -14,7 +14,7 @@ namespace GenericTest
         {
             GenericCheck<int> obj = new GenericCheck<int>();
             int actual = (obj.Max(22, 33, 90));
-            Assert.Equal(90,actual);
+            Assert.Equal(90, actual);
         }
         /// <summary>
         /// Checking for Generic Float datatype
@@ -35,6 +35,22 @@ namespace GenericTest
             GenericCheck<String> obj = new GenericCheck<String>();
             String actual = (obj.Max("26", "33", "82"));
             Assert.Equal("82", actual);
+        }
+        [Fact]
+        public void GeneticMaximumTest()
+        {
+            GenericCheck<int> obj1 = new GenericCheck<int>();
+            GenericCheck<double> obj2 = new GenericCheck<double>();
+            GenericCheck<String> obj3 = new GenericCheck<String>();
+            int[] arr1 = { 88, 8, 56, 75 };
+            double[] arr2 = { 8.8, 1.2, 5.6, 7.5 };
+            String[] arr3 = { "88", "8", "56", "75" };
+            int actual1 = (obj1.Max(arr1));
+            double actual2 = (obj2.Max(arr2));
+            String actual3 = (obj3.Max(arr3));
+            Assert.Equal(88, actual1);
+            Assert.Equal(8.8, actual2);
+            Assert.Equal("88", actual3);
         }
     }
 }
